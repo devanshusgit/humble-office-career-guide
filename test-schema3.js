359c560cda3e077d0f3eb6627dff12018608c6b9
PSF <psf@PSFs-Mac-mini.local>
2026-07-25 03:49:54 +0530

    Fix build: remove temporary debug scripts

--- test-schema3.js
+++
@@ -1,6 +0,0 @@
-const { z } = require('zod');
-const { zodToJsonSchema } = require('zod-to-json-schema');
-const CareerGuideResultSchema = z.object({
-  guideTitle: z.string(),
-});
-console.log(JSON.stringify(zodToJsonSchema(CareerGuideResultSchema, "mySchema"), null, 2));
--- test-schema4.ts
+++
@@ -1,4 +0,0 @@
-import { z } from 'zod';
-import { zodToJsonSchema } from 'zod-to-json-schema';
-import { CareerGuideResultSchema } from './lib/gemini/schema';
-console.log(JSON.stringify(zodToJsonSchema(CareerGuideResultSchema), null, 2));
